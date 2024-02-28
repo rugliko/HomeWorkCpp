9d81744f8e54f1aa42bd0455ff7179d401252de3
rugliko <159914751+rugliko@users.noreply.github.com>
2024-02-28 14:17:10 +0300

    Add files via upload

---
+++ 13.03.2024/TopHomeWorkCpp.cpp
@@ -0,0 +1,89 @@
+﻿
+
+#include <iostream>
+
+ 
+int main()
+
+{
+    selocale(LC_ALL, "");
+    std::cout << "Пушкин А.С. - Туча\n";
+    std::cout << "Последняя туча рассеянной бури!\n";
+    std::cout << "Одна ты несешься по ясной лазури,\n";
+    std::cout << "Одна ты наводишь унылую тень,\n";
+    std::cout << "Одна ты печалишь ликующий день\n";
+    std::cout << "Ты небо недавно кругом облегала,\n";
+    std::cout << "И молния грозно тебя обвивала\n";
+    std::cout << "И ты издавала таинственный гром\n";
+    std::cout << "И алчную землю поила дождем.\n";
+    std::cout << "Довольно, сокройся! Пора миновалась\n";
+    std::cout << "Земля освежилась, и буря промчалась,\n";
+    std::cout << "И ветер, лаская листочки древес,\n";
+    std::cout << "Тебя с успокоенных гонит небес.\n";
+    std::cout << "Every" << "\n";
+    std::cout << "\tHunter" << "\n";
+    std::cout << "\t\tWants" << "\n";
+    std::cout << "\t\t\tTo know" << "\n";
+    std::cout << "\t\t\t\t Where" << "\n";
+    std::cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+   std::cout << "\t\t\t\t\t\t\tSit" << "\n\n";
+    std::cout << "\t\t\tsell sneakers\n";
+    std::cout << "\t\t\tmodel: sb dunk low \n";
+    std::cout << "\t\t\tsize: 46 \n";
+    std::cout << "\t\t\tphone number:222-22-222\n";
+    std::cout << "\t\t\t222\t|22-22|\n";
+    std::cout << "\t\t\t222\t|22-22|\n";
+    std::cout << "\t\t\t222\t|22-22|\n";
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
