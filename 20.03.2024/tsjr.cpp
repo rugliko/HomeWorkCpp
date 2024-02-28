0aec542ef599f473248a71716134512441073072
rugliko <159914751+rugliko@users.noreply.github.com>
2024-02-28 15:40:32 +0300

    Add files via upload

---
+++ 20.03.2024/tsjr.cpp
@@ -0,0 +1,120 @@
+﻿
+
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    //задача 1
+    //setlocale(LC_ALL, "Russian");
+    //int a,a1,a2,a3,a4,a5,a6;
+   // cin >> a;
+    //a1 = a / 100000;
+   // a2 = (a % 100000) / 10000;
+   // a3 = ((a % 100000) % 10000) / 1000;
+   // a4 = (((a % 100000) % 10000) % 1000) / 100;
+   // a5 = ((((a % 100000) % 10000) % 1000) % 100) / 10;
+   // a6 = (((((a % 100000) % 10000) % 1000) % 100) % 10);
+  //    if (a > 999999) {
+   //     cout << "eror";
+  // }
+  //    else if (a1 + a2 + a3 == a4 + a5 + a6) {
+  //      cout << "lucky";
+//
+  //  }
+  //    else if (a1 + a3 + a3 != a4 + a5 + a6) {
+  //      cout << "unlucky";
+    //задача 2 
+   // int b, a, a1, a2, a3, a4;
+   // cin >> a;
+   // a1 = a / 1000*100;
+   // a2 = (a % 1000) / 100*1000;
+   // a3 = ((a % 1000) % 100) / 10;
+   // a4 = ((a % 1000) % 100) % 10*10;
+   // if (a > 9999) {
+   //     cout << "eror";
+  //  }
+  //  b = a1 + a2 + a3 + a4;
+  //  cout << b;
+// задача 3 
+  //  int a, b, c, d, e, f, g ;
+  //  cin >> a >> b >> c >> d >> e >> f >> g;
+  //  if (a < b) {
+  //      a = b;
+  //  }
+  //  if (a < c) {
+  //      a = c;
+ //   }
+ //   if (a < d) {
+  //      a = d;
+ //   }
+ //   if (a < e) {
+ //       a = e;
+ //   }
+ //   if (a < f) {
+ //       a = f;
+ //   }
+ //   if (a < g) {
+ //       a = g;
+ //   }
+ //   cout << a;
+//задача4
+    int tank = 300, distanceAB, distanseBC, weight;
+    cin >> weight;
+    if (weight > 2000) {
+        exit;
+    }
+    cin >> distanceAB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+   
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
